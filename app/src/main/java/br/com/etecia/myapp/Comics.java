535df639c62a95bebcaa04e70f368e19b311fa29
maarialara6 <eduardalara5921@gmail.com>
2026-06-11 20:04:34 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/myapp/Comics.java
@@ -0,0 +1,13 @@
+package br.com.etecia.myapp;
+
+public class Comics {
+    private String universo;
+    private String titulo;
+    private int imgComic;
+
+    public String getTitulo(){
+        return titulo;
+    }
+
+    public void setTitulo(String )
+}
