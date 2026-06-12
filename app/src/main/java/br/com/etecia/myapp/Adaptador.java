ceb74c998972f28a60b148ad438602ee25913553
maarialara6 <eduardalara5921@gmail.com>
2026-06-11 21:13:23 -0300

    Initial commit

--- app/src/main/java/br/com/etecia/myapp/Adaptador.java
+++ app/src/main/java/br/com/etecia/myapp/Adaptador.java
@@ -1,12 +1,19 @@
 package br.com.etecia.myapp;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class Adaptador extends RecyclerView.Adapter<Adaptador.ViewHolder> {
+
+    private Context context;
+    private List<Comics> lstcomic;
+
     public Adaptador() {
     }
 
