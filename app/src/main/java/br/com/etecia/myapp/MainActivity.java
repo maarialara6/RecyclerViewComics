cbd0b19b9e7e229ebe9230152c5b25dffc1319a6
maarialara6 <eduardalara5921@gmail.com>
2026-06-11 21:47:21 -0300

    Initial commit

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -7,8 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    List<Comics> lstcomic;
+    RecyclerView idRecDc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
